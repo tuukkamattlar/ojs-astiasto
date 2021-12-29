f3482098bb9b2694df7c83b2934a5a75c0c3b9b0
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2021-12-29 08:02:43 +0200

    update

--- pages/about.js
+++ pages/about.js
@@ -13,46 +13,28 @@ export default function About() {
   const HomeName = {'FI': 'Koti','EN': 'Home'}
   const AboutName = {'FI': 'Astiastosta','EN': 'About'}
 
-  function encode(data) {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&")
-  }
-
-const handleSubmit = (event) => {
-  event.preventDefault()
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode({
-      "form-name": event.target.getAttribute("name"),
-      ...name
-    })
-  }).then(() => alert("Done")).catch(error => alert(error))
-}
-
   return (
     <div>
-      <form name="try" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit" onClick={()=>handleSubmit()}>Send</button>
-  </p>
-</form>
+      <form name="try" action="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
     </div>
   )
 }
