980444541e24d1e93ff3a0ac51c73a673ff2d211
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2021-12-29 07:56:03 +0200

    update

--- pages/about.js
+++ pages/about.js
@@ -7,15 +7,52 @@ import Container from 'react-bootstrap/Container'
 import { useState } from 'react'
 
 
-export default function Home() {
+export default function About() {
   const [language, setLanguage] = useState('FI')
   const SiteName = {'FI': 'OJS astiasto','EN': 'OJS tableware'}
   const HomeName = {'FI': 'Koti','EN': 'Home'}
   const AboutName = {'FI': 'Astiastosta','EN': 'About'}
 
+  function encode(data) {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&")
+  }
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encode({
+      "form-name": event.target.getAttribute("name"),
+      ...name
+    })
+  }).then(() => alert("Done")).catch(error => alert(error))
+}
+
   return (
     <div>
-      deezz nuts
+      <form name="try" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit" onClick={()=>handleSubmit()}>Send</button>
+  </p>
+</form>
     </div>
   )
 }
