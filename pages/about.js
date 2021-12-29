43ff459791f32032aa54de19d74272f61715d0a6
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2021-12-29 08:11:18 +0200

    update

--- pages/about.js
+++ pages/about.js
@@ -15,18 +15,13 @@ export default function About() {
 
   return (
     <div>
-      <form name="try" action="POST" data-netlify="true">
+      <form name="try" method="post" data-netlify="true" onSubmit="submit">
+        <input type="hidden" name="form-name" value="try2"/>
       <p>
         <label>Your Name: <input type="text" name="name" /></label>   
       </p>
       <p>
         <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Your Role: <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label>
       </p>
       <p>
         <label>Message: <textarea name="message"></textarea></label>
